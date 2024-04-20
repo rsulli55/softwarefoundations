9f5d37f3139ab95887d2758ca924857973f273e3
Ryan Sullivant <rsulli55@gmail.com>
2024-04-20 17:08:01 -0600

    Finish Lists.v. Add scripts to encrypt and decrypt solutions. Replace solutions with encrypted versions.

---
+++ lf/decrypt_solutions.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+files=("Basics.v" "Induction.v" "Lists.v" "Poly.v")
+
+for file in "${files[@]}"; do
+  printf "Decrypting %s\n" "${file}.gpg"
+  gpg --batch --passphrase `cat pass.txt` -o "$file" --decrypt "$file.gpg"
+done 
---
+++ lf/encrypt_solutions.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+files=("Basics.v" "Induction.v" "Lists.v" "Poly.v")
+
+for file in "${files[@]}"; do
+  printf "Encrypting %s\n" ${file}
+  gpg --batch --passphrase `cat pass.txt` -o "$file.gpg" --symmetric $file
+done 
+
